7033c1c0db80a241918b9a2807e60db86184ef6e
mnaofficialbd <mna.official92@gmail.com>
2025-02-16 22:45:19 +0600

    frontend file structure update

---
+++ frontend/config.js
@@ -0,0 +1,5 @@
+export const BASE_URL = "http://localhost:5000/api/v1";
+
+// export const BASE_URL = "https://mna-epic-photographer-backend.onrender.com/api/v1";
+
+export const token = localStorage.getItem("token");
