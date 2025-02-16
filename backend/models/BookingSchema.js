8342bb36dd97bf47eafd7b2a2c58080675b8b137
mnaofficialbd <mna.official92@gmail.com>
2025-02-16 21:53:53 +0600

    booking-schema model update

--- backend/models/BookingSchema.js
+++ backend/models/BookingSchema.js
@@ -1,30 +1,35 @@
 import mongoose from "mongoose";
 
-const BookingSchema = new mongoose.Schema(
-  {
-    photographer: {
-      type: mongoose.Types.ObjectId,
-      ref: "Photographer",
-      required: true,
-    },
-    user: {
-      type: mongoose.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    servicePrice: { type: String, required: true },
-    status: {
-      type: String,
-      enum: ["pending", "approved", "cancelled"],
-      default: "pending",
-    },
-    isPaid: {
-      type: Boolean,
-      default: true,
-    },
+const BookingSchema = new mongoose.Schema({
+  photographer: {
+    type: mongoose.Types.ObjectId,
+    ref: "Photographer",
+    required: true,
   },
-  { timestamps: true }
-);
+  user: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  servicePrice: {
+    type: Number,
+    required: [true, "Amount is required"],
+    min: [0, "Amount cannot be negative"],
+  },
+  status: {
+    type: String,
+    enum: ["pending", "approved", "cancelled"],
+    default: "pending",
+  },
+  isPaid: {
+    type: Boolean,
+    default: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 BookingSchema.pre(/^find/, function (next) {
   this.populate("user").populate({
