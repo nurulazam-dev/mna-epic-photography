db9cf25bb40eeb9607e8d0fde17eacabe1dc715a
mnaofficialbd <mna.official92@gmail.com>
2025-02-16 21:50:27 +0600

    user-schema model update

--- backend/models/UserSchema.js
+++ backend/models/UserSchema.js
@@ -1,30 +1,36 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema(
-  {
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true, minlength: 6, select: false },
-    name: { type: String, required: true, trim: true },
-    phone: { type: String },
-    photo: { type: String },
-    role: {
-      type: String,
-      enum: ["admin", "photographer", "client"],
-      default: "client",
-    },
-    bookings: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Booking",
-      },
-    ],
-    isVerified: {
-      type: Boolean,
-      default: false,
-    },
+const UserSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 6, select: false },
+  name: { type: String, required: true, trim: true },
+  phone: {
+    type: String,
+    required: [true, "Phone number is required"],
+    unique: [true, "Phone number is unique"],
+    match: [/^[0-9]{11}$/, "Phone number isn't 11 characters"],
   },
-  { timestamps: true }
-);
+  photo: { type: String },
+  role: {
+    type: String,
+    enum: ["admin", "photographer", "client"],
+    default: "client",
+  },
+  bookings: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Booking",
+    },
+  ],
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 UserSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
