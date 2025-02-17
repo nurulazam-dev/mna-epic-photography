9bf8bcd87ca831a2f6ba3b17906f0139bb65272e
mnaofficialbd <mna.official92@gmail.com>
2025-02-17 22:29:31 +0600

    ProtectedRoutes setup

--- frontend/src/Routes/ProtectedRoutes.jsx
+++ frontend/src/Routes/ProtectedRoutes.jsx
@@ -1,5 +1,15 @@
-const ProtectedRoutes = () => {
-  return <div></div>;
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { authContext } from "../context/AuthContext";
+
+const ProtectedRoutes = ({ children, allowedRoles }) => {
+  const { token, role } = useContext(authContext);
+
+  const isAllowed = allowedRoles.includes(role);
+  const accessibleRoute =
+    token && isAllowed ? children : <Navigate to="/login" replace={true} />;
+
+  return accessibleRoute;
 };
 
 export default ProtectedRoutes;
