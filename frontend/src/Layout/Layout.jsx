62333f245645a5fa29e5bac496e5eeb79b38f487
mnaofficialbd <mna.official92@gmail.com>
2025-02-17 22:36:04 +0600

    layout update

--- frontend/src/Layout/Layout.jsx
+++ frontend/src/Layout/Layout.jsx
@@ -1,11 +1,14 @@
 import Footer from "../components/Shared/Footer";
 import Header from "../components/Shared/Header";
+import Routers from "../Routes/Routers";
 
 const Layout = () => {
   return (
     <>
       <Header />
-      <main>Routers</main>
+      <main>
+        <Routers />
+      </main>
       <Footer />
     </>
   );
