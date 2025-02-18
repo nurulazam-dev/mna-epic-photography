e5a1e15f83b8c3125a543a5517688339628583f9
mnaofficialbd <mna.official92@gmail.com>
2025-02-18 22:50:32 +0600

    routers update

--- frontend/src/Routes/Routers.jsx
+++ frontend/src/Routes/Routers.jsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../Pages/Home";
 import NotFound from "../Pages/NotFound";
+import Login from "../Pages/Login";
+import Register from "../Pages/Register";
 
 const Routers = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
 
       <Route path="*" element={<NotFound />} />
     </Routes>
