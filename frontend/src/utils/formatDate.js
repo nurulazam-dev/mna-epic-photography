9d9630cce7d5b078af9169056b09fed54e584545
mnaofficialbd <mna.official92@gmail.com>
2025-02-21 00:00:18 +0600

    date-formetter create

---
+++ frontend/src/utils/formatDate.js
@@ -0,0 +1,5 @@
+export const formatDate = (date, config) => {
+  const defaultOptions = { day: "2-digit", month: "2-digit", year: "numeric" };
+  const options = config ? config : defaultOptions;
+  return new Date(date).toLocaleDateString("en-GB", options);
+};
