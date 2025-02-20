5647426d9c7c94fcef7d6f9418e969fc08716f36
mnaofficialbd <mna.official92@gmail.com>
2025-02-21 00:27:51 +0600

    FeedbackForm update

--- frontend/src/components/Photographers/PhotographersDetails/FeedbackForm.jsx
+++ frontend/src/components/Photographers/PhotographersDetails/FeedbackForm.jsx
@@ -1,5 +1,98 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import { BASE_URL, token } from "../../../../config";
+import Loading from "../../Shared/Loading";
+import { Box, Typography, Button, TextField, Rating } from "@mui/material";
+
 const FeedbackForm = () => {
-  return <div>FeedbackForm</div>;
+  const [rating, setRating] = useState(0);
+  const [reviewText, setReviewText] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const { id } = useParams();
+
+  const handleSubmitReview = async (e) => {
+    e.preventDefault();
+    if (!rating || !reviewText)
+      return toast.error("Rating & Review are required");
+
+    setLoading(true);
+    try {
+      const res = await fetch(`${BASE_URL}/photographers/${id}/reviews`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ rating, reviewText }),
+      });
+
+      const result = await res.json();
+      if (!res.ok) throw new Error(result.message);
+
+      setRating(0);
+      setReviewText("");
+      toast.success(result.message);
+    } catch (err) {
+      toast.error(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Box
+      component="form"
+      onSubmit={handleSubmitReview}
+      sx={{
+        maxWidth: 500,
+        mx: "auto",
+        p: 3,
+        bgcolor: "white",
+        boxShadow: 3,
+        borderRadius: 2,
+      }}
+    >
+      {/* Rating Section */}
+      <Typography variant="h6" sx={{ mb: 2 }}>
+        How would you rate the photographer?
+      </Typography>
+      <Rating
+        name="rating"
+        value={rating}
+        precision={1}
+        onChange={(_, newValue) => setRating(newValue)}
+        sx={{ mb: 3 }}
+      />
+
+      {/* Feedback Input */}
+      <Typography variant="h6" sx={{ mb: 2 }}>
+        Share your feedback or suggestions.
+      </Typography>
+      <TextField
+        fullWidth
+        multiline
+        rows={4}
+        placeholder="Write your feedback..."
+        value={reviewText}
+        onChange={(e) => setReviewText(e.target.value)}
+        variant="outlined"
+      />
+
+      {/* Submit Button */}
+      <Button
+        type="submit"
+        variant="contained"
+        color="primary"
+        fullWidth
+        sx={{ mt: 3 }}
+        disabled={loading}
+      >
+        {loading ? <Loading /> : "Submit Feedback"}
+      </Button>
+    </Box>
+  );
 };
 
 export default FeedbackForm;
