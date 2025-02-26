a55a620a85cb83b74b5b4d6af46d005703d9e241
mnaofficialbd <mna.official92@gmail.com>
2025-02-26 08:56:16 +0600

    test code setup

--- frontend/src/Layout/Layout.jsx
+++ frontend/src/Layout/Layout.jsx
@@ -1,11 +1,13 @@
 import Footer from "../components/Shared/Footer";
-import Header from "../components/Shared/Header";
+// import Header from "../components/Shared/Header";
+import TestCode from "../components/Shared/TestCode";
 import Routers from "../Routes/Routers";
 
 const Layout = () => {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
+      <TestCode />
       <main>
         <Routers />
       </main>
