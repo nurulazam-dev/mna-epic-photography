96f44c4d7927e3a8be7a604df29f1b66e3b7cbb0
mnaofficialbd <mna.official92@gmail.com>
2025-03-07 18:43:56 +0600

    user-routes update

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -12,11 +12,16 @@ import { authenticate, restrict } from "../auth/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/:id", authenticate, restrict(["client"]), getSingleUser);
+router.get("/:id", authenticate, restrict(["client", "admin"]), getSingleUser);
 router.get("/", authenticate, restrict(["admin"]), getAllUser);
-router.put("/:id", authenticate, restrict(["client"]), updateUser);
-router.delete("/:id", authenticate, restrict(["client"]), deleteUser);
-router.get("/profile/me", authenticate, restrict(["client"]), getUserProfile);
+router.put("/:id", authenticate, restrict(["client", "admin"]), updateUser);
+router.delete("/:id", authenticate, restrict(["client", "admin"]), deleteUser);
+router.get(
+  "/profile/me",
+  authenticate,
+  restrict(["client", "admin"]),
+  getUserProfile
+);
 router.get(
   "/booking/my-bookings",
   authenticate,
