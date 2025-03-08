94b11adce15cc8fb6654df48931f1ef97089ea6f
mnaofficialbd <mna.official92@gmail.com>
2025-03-08 21:20:06 +0600

    manage users & photographers create and setup

---
+++ frontend/src/Dashboard/Admin/ManagePhotographers.jsx
@@ -0,0 +1,7 @@
+import { Typography } from "@mui/material";
+
+const ManagePhotographers = () => {
+  return <Typography variant="h4">User Management Page</Typography>;
+};
+
+export default ManagePhotographers;
---
+++ frontend/src/Dashboard/Admin/ManageUsers.jsx
@@ -0,0 +1,7 @@
+import { Typography } from "@mui/material";
+
+const ManageUsers = () => {
+  return <Typography variant="h4">User Management Page</Typography>;
+};
+
+export default ManageUsers;
