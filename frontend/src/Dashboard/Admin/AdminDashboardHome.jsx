3cb27d90840ef68e09603d9e80f010c844f43543
mnaofficialbd <mna.official92@gmail.com>
2025-03-08 21:17:00 +0600

    AdminDashboardHome create and setup

---
+++ frontend/src/Dashboard/Admin/AdminDashboardHome.jsx
@@ -0,0 +1,7 @@
+import { Typography } from "@mui/material";
+
+const AdminDashboardHome = () => {
+  return <Typography variant="h4">Welcome to Admin Dashboard</Typography>;
+};
+
+export default AdminDashboardHome;
