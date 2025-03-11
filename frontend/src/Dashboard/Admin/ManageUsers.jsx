2101be4df58658b05ad7ed765460b0382bb24ba7
mnaofficialbd <mna.official92@gmail.com>
2025-03-11 21:17:50 +0600

    manage-users update

--- frontend/src/Dashboard/Admin/ManageUsers.jsx
+++ frontend/src/Dashboard/Admin/ManageUsers.jsx
@@ -1,7 +1,26 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 const ManageUsers = () => {
-  return <Typography variant="h4">Manage Users Page</Typography>;
+  return (
+    <Box>
+      <Box>
+        <Typography
+          variant="h4"
+          align="center"
+          gutterBottom
+          sx={{
+            backgroundColor: "#2E7D32",
+            color: "white",
+            py: 1,
+            fontFamily: "serif",
+            borderRadius: 1,
+          }}
+        >
+          Manage Users
+        </Typography>
+      </Box>
+    </Box>
+  );
 };
 
 export default ManageUsers;
