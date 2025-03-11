d68682b42b03db4259ae62e8720bbfb9c36d7771
mnaofficialbd <mna.official92@gmail.com>
2025-03-11 21:06:41 +0600

    AdminProfile update

--- frontend/src/Dashboard/Admin/AdminProfile.jsx
+++ frontend/src/Dashboard/Admin/AdminProfile.jsx
@@ -1,4 +1,25 @@
-const AdminProfile = () => {
+import { useEffect, useState } from "react";
+import {
+  Button,
+  TextField,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
+  Typography,
+  // Container,
+  Avatar,
+  CircularProgress,
+  Box,
+  Grid,
+} from "@mui/material";
+import { CloudUpload, Update } from "@mui/icons-material";
+import { toast } from "react-toastify";
+import { BASE_URL, token } from "../../../config";
+import uploadImageToCloudinary from "../../utils/uploadCloudinary";
+import Loading from "../../components/Shared/Loading";
+
+const AdminProfile = ({ adminData }) => {
   return <div>AdminProfile</div>;
 };
 
