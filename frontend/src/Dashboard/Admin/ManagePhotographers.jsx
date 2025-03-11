2d0fe2e1a3b5b4b35315bd9508b1dab8a73931f4
mnaofficialbd <mna.official92@gmail.com>
2025-03-11 21:18:57 +0600

    ManagePhotographers update

--- frontend/src/Dashboard/Admin/ManagePhotographers.jsx
+++ frontend/src/Dashboard/Admin/ManagePhotographers.jsx
@@ -1,7 +1,26 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 const ManagePhotographers = () => {
-  return <Typography variant="h4">Manage Photographers Page</Typography>;
+  return (
+    <Box>
+      <Box>
+        <Typography
+          variant="h4"
+          align="center"
+          gutterBottom
+          sx={{
+            backgroundColor: "#2E7D32",
+            color: "white",
+            py: 1,
+            fontFamily: "serif",
+            borderRadius: 1,
+          }}
+        >
+          Manage Photographers
+        </Typography>
+      </Box>
+    </Box>
+  );
 };
 
 export default ManagePhotographers;
