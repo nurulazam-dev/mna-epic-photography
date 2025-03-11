f5fc83192152ee3df0f37ed340733c02b56c9510
mnaofficialbd <mna.official92@gmail.com>
2025-03-11 21:34:40 +0600

    ManageBookings update

--- frontend/src/Dashboard/Admin/ManageBookings.jsx
+++ frontend/src/Dashboard/Admin/ManageBookings.jsx
@@ -1,24 +1,146 @@
-import { Box, Typography } from "@mui/material";
+/* eslint-disable react/prop-types */
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Typography,
+  Avatar,
+  Box,
+  // Container,
+} from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import CancelIcon from "@mui/icons-material/Cancel";
+import { formatDate } from "../../utils/formatDate";
+import { GppBad, VerifiedUser } from "@mui/icons-material";
 
-const ManageBookings = () => {
+const ManageBookings = ({ bookings }) => {
   return (
     <Box>
-      <Box>
+      <Typography
+        variant="h4"
+        align="center"
+        gutterBottom
+        sx={{
+          backgroundColor: "#2E7D32",
+          color: "white",
+          py: 1,
+          fontFamily: "serif",
+          borderRadius: 1,
+        }}
+      >
+        Manage Bookings
+      </Typography>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow sx={{ backgroundColor: "#f3f3f3" }}>
+              <TableCell align="center">Client</TableCell>
+              <TableCell align="center">Phone</TableCell>
+              <TableCell align="center">Photogs</TableCell>
+              <TableCell align="center">Payment</TableCell>
+              <TableCell align="center">Status</TableCell>
+              <TableCell align="center">Price</TableCell>
+              <TableCell align="center">Booked on</TableCell>
+              <TableCell align="center">Program</TableCell>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            {bookings?.map((item) => (
+              <TableRow key={item?._id} hover>
+                <TableCell>
+                  <Box display="flex" alignItems="center" gap={2}>
+                    <Avatar src={item?.user?.photo} alt={item?.user?.name} />
+                    <Box>
+                      <Typography fontWeight="bold" display="flex">
+                        {item?.user?.name}
+                        {item?.user?.isVerified === true ? (
+                          <Box
+                            display="flex"
+                            alignItems="center"
+                            justifyContent="center"
+                            color="green"
+                            height={16}
+                            width={16}
+                            sx={{ marginLeft: "3px" }}
+                          >
+                            <VerifiedUser fontSize="10px" />
+                            {/* <CheckCircleIcon fontSize="10px" /> */}
+                          </Box>
+                        ) : (
+                          <Box
+                            display="flex"
+                            alignItems="center"
+                            justifyContent="center"
+                            color="red"
+                            height={16}
+                            width={16}
+                            sx={{ marginLeft: "3px" }}
+                          >
+                            <GppBad fontSize="10px" />
+                            {/* <CancelIcon fontSize="10px" /> */}
+                          </Box>
+                        )}
+                      </Typography>
+                      <Typography variant="body2" color="textSecondary">
+                        {item?.user?.email}
+                      </Typography>
+                    </Box>
+                  </Box>
+                </TableCell>
+
+                <TableCell align="center">{item?.user?.phone}</TableCell>
+
+                <TableCell align="center">
+                  {item?.isPaid ? (
+                    <Box
+                      display="flex"
+                      alignItems="center"
+                      justifyContent="center"
+                      color="green"
+                    >
+                      <CheckCircleIcon sx={{ mr: 1 }} /> Paid
+                    </Box>
+                  ) : (
+                    <Box
+                      display="flex"
+                      alignItems="center"
+                      justifyContent="center"
+                      color="red"
+                    >
+                      <CancelIcon sx={{ mr: 1 }} /> Unpaid
+                    </Box>
+                  )}
+                </TableCell>
+
+                <TableCell align="center">{item?.status}</TableCell>
+                <TableCell align="center">${item?.servicePrice}</TableCell>
+
+                <TableCell align="center">
+                  {formatDate(item?.createdAt)}
+                </TableCell>
+                <TableCell align="center">
+                  {formatDate(item?.programDate)}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      {bookings?.length === 0 && (
         <Typography
-          variant="h4"
-          align="center"
-          gutterBottom
-          sx={{
-            backgroundColor: "#2E7D32",
-            color: "white",
-            py: 1,
-            fontFamily: "serif",
-            borderRadius: 1,
-          }}
+          variant="h6"
+          color="error"
+          sx={{ textAlign: "center", mt: 2, animation: "pulse 1s infinite" }}
         >
-          Manage Bookings
+          No bookings available.
         </Typography>
-      </Box>
+      )}
     </Box>
   );
 };
