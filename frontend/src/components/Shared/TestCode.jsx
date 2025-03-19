17f1f7cf4b249229b216b56dbc93d5fd3877930e
mnaofficialbd <mna.official92@gmail.com>
2025-03-19 22:04:10 +0600

    GallerySection create

--- frontend/src/components/Shared/TestCode.jsx
+++ frontend/src/components/Shared/TestCode.jsx
@@ -3,46 +3,8 @@ import img1 from "../../assets/images/eventPhotos/2.png";
 import img2 from "../../assets/images/eventPhotos/3.png";
 import img3 from "../../assets/images/eventPhotos/4.png";
 import img4 from "../../assets/images/eventPhotos/5.png";
-// import img5 from "../../assets/images/eventPhotos/6.png";
-// import img6 from "../../assets/images/eventPhotos/7.png";
-
-/* const images = [
-  {
-    src: img1,
-    cols: 6,
-    rows: 2,
-  },
-  {
-    src: img2,
-    cols: 3,
-    rows: 1,
-  },
-  {
-    src: img3,
-    cols: 3,
-    rows: 1,
-  },
-  {
-    src: img4,
-    cols: 6,
-    rows: 1,
-  },
-   {
-    src: img5,
-    cols: 4,
-    rows: 2,
-  },
-  {
-    src: img6,
-    cols: 4,
-    rows: 2,
-  },
-  {
-    src: img1,
-    cols: 4,
-    rows: 2,
-  }, 
-]; */
+import img5 from "../../assets/images/eventPhotos/6.png";
+import img6 from "../../assets/images/eventPhotos/7.png";
 
 const TestCode = () => {
   return (
@@ -51,52 +13,27 @@ const TestCode = () => {
         Our Gallery test
       </Typography>
 
-      {/* <Grid container spacing={2}>
-        {images.map((img, index) => (
-          <Grid
-            item
-            key={index}
-            xs={12}
-            sm={img.cols * 2}
-            md={img.cols}
-            lg={img.cols}
-          >
-            <Card
-              sx={{
-                position: "relative",
-                height: img.rows * 170,
-                borderRadius: 1,
-                overflow: "hidden",
-              }}
-            >
-              <CardMedia
-                component="img"
-                height="100%"
-                image={img.src}
-                alt={img.title}
-                sx={{ objectFit: "cover", width: "100%" }}
-              />
-            </Card>
-          </Grid>
-        ))}
-      </Grid> */}
-
-      <Grid container spacing={2}>
-        {/* 1st Image (Large - 6 cols, 2 rows) */}
-        <Grid item xs={12} sm={6} md={6} lg={6} sx={{ gridRow: "span 2" }}>
+      <Grid
+        container
+        sx={{
+          display: "grid",
+          gridTemplateColumns: "repeat(12, 1fr)",
+          gridTemplateRows: "repeat(2, auto)",
+          gap: 3,
+        }}
+      >
+        <Box sx={{ gridColumn: "span 6", gridRow: "span 2" }}>
           <Card sx={{ height: "100%", borderRadius: 2, overflow: "hidden" }}>
             <CardMedia
               component="img"
-              height="100%"
               image={img1}
               alt="Gallery Image 1"
               sx={{ objectFit: "cover", width: "100%", height: "100%" }}
             />
           </Card>
-        </Grid>
+        </Box>
 
-        {/* 2nd Image (Small - 3 cols, 1 row) */}
-        <Grid item xs={12} sm={3} md={3} lg={3}>
+        <Box sx={{ gridColumn: "span 3", gridRow: "span 1" }}>
           <Card sx={{ borderRadius: 2, overflow: "hidden" }}>
             <CardMedia
               component="img"
@@ -105,10 +42,9 @@ const TestCode = () => {
               sx={{ objectFit: "cover", width: "100%", height: "100%" }}
             />
           </Card>
-        </Grid>
+        </Box>
 
-        {/* 3rd Image (Small - 3 cols, 1 row) */}
-        <Grid item xs={12} sm={3} md={3} lg={3}>
+        <Box sx={{ gridColumn: "span 3", gridRow: "span 1" }}>
           <Card sx={{ borderRadius: 2, overflow: "hidden" }}>
             <CardMedia
               component="img"
@@ -117,10 +53,9 @@ const TestCode = () => {
               sx={{ objectFit: "cover", width: "100%", height: "100%" }}
             />
           </Card>
-        </Grid>
+        </Box>
 
-        {/* 4th Image (Corrected placement - Below 2nd & 3rd, beside 1st Image) */}
-        <Grid item xs={12} sm={6} md={6} lg={6}>
+        <Box sx={{ gridColumn: "span 6", gridRow: "span 1" }}>
           <Card sx={{ borderRadius: 2, overflow: "hidden" }}>
             <CardMedia
               component="img"
@@ -129,7 +64,48 @@ const TestCode = () => {
               sx={{ objectFit: "cover", width: "100%", height: "100%" }}
             />
           </Card>
-        </Grid>
+        </Box>
+      </Grid>
+      <Grid
+        container
+        mt={2}
+        sx={{
+          display: "grid",
+          gridTemplateColumns: "repeat(12, 1fr)",
+          gridTemplateRows: "repeat(3, auto)",
+          gap: 3,
+        }}
+      >
+        <Box sx={{ gridColumn: "span 4", gridRow: "span 1" }}>
+          <Card sx={{ borderRadius: 2, overflow: "hidden" }}>
+            <CardMedia
+              component="img"
+              image={img5}
+              alt="Gallery Image 4"
+              sx={{ objectFit: "cover", width: "100%", height: "100%" }}
+            />
+          </Card>
+        </Box>
+        <Box sx={{ gridColumn: "span 4", gridRow: "span 1" }}>
+          <Card sx={{ borderRadius: 2, overflow: "hidden" }}>
+            <CardMedia
+              component="img"
+              image={img6}
+              alt="Gallery Image 4"
+              sx={{ objectFit: "cover", width: "100%", height: "100%" }}
+            />
+          </Card>
+        </Box>
+        <Box sx={{ gridColumn: "span 4", gridRow: "span 1" }}>
+          <Card sx={{ borderRadius: 2, overflow: "hidden" }}>
+            <CardMedia
+              component="img"
+              image={img1}
+              alt="Gallery Image 4"
+              sx={{ objectFit: "cover", width: "100%", height: "100%" }}
+            />
+          </Card>
+        </Box>
       </Grid>
     </Box>
   );
