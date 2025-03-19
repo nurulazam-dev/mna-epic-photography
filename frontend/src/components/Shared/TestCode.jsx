05fe8dbaf4d460d97c6ccb349d5ee4aad6717692
mnaofficialbd <mna.official92@gmail.com>
2025-03-19 21:36:42 +0600

    GallerySection create

--- frontend/src/components/Shared/TestCode.jsx
+++ frontend/src/components/Shared/TestCode.jsx
@@ -3,10 +3,10 @@ import img1 from "../../assets/images/eventPhotos/2.png";
 import img2 from "../../assets/images/eventPhotos/3.png";
 import img3 from "../../assets/images/eventPhotos/4.png";
 import img4 from "../../assets/images/eventPhotos/5.png";
-import img5 from "../../assets/images/eventPhotos/6.png";
-import img6 from "../../assets/images/eventPhotos/7.png";
+// import img5 from "../../assets/images/eventPhotos/6.png";
+// import img6 from "../../assets/images/eventPhotos/7.png";
 
-const images = [
+/* const images = [
   {
     src: img1,
     cols: 6,
@@ -27,7 +27,7 @@ const images = [
     cols: 6,
     rows: 1,
   },
-  {
+   {
     src: img5,
     cols: 4,
     rows: 2,
@@ -41,17 +41,17 @@ const images = [
     src: img1,
     cols: 4,
     rows: 2,
-  },
-];
+  }, 
+]; */
 
 const TestCode = () => {
   return (
     <Box sx={{ p: 3 }}>
       <Typography variant="h4" fontWeight="bold" textAlign="center" mb={3}>
-        Our Gallery
+        Our Gallery test
       </Typography>
 
-      <Grid container spacing={2}>
+      {/* <Grid container spacing={2}>
         {images.map((img, index) => (
           <Grid
             item
@@ -79,6 +79,57 @@ const TestCode = () => {
             </Card>
           </Grid>
         ))}
+      </Grid> */}
+
+      <Grid container spacing={2}>
+        {/* 1st Image (Large - 6 cols, 2 rows) */}
+        <Grid item xs={12} sm={6} md={6} lg={6} sx={{ gridRow: "span 2" }}>
+          <Card sx={{ height: "100%", borderRadius: 2, overflow: "hidden" }}>
+            <CardMedia
+              component="img"
+              height="100%"
+              image={img1}
+              alt="Gallery Image 1"
+              sx={{ objectFit: "cover", width: "100%", height: "100%" }}
+            />
+          </Card>
+        </Grid>
+
+        {/* 2nd Image (Small - 3 cols, 1 row) */}
+        <Grid item xs={12} sm={3} md={3} lg={3}>
+          <Card sx={{ borderRadius: 2, overflow: "hidden" }}>
+            <CardMedia
+              component="img"
+              image={img2}
+              alt="Gallery Image 2"
+              sx={{ objectFit: "cover", width: "100%", height: "100%" }}
+            />
+          </Card>
+        </Grid>
+
+        {/* 3rd Image (Small - 3 cols, 1 row) */}
+        <Grid item xs={12} sm={3} md={3} lg={3}>
+          <Card sx={{ borderRadius: 2, overflow: "hidden" }}>
+            <CardMedia
+              component="img"
+              image={img3}
+              alt="Gallery Image 3"
+              sx={{ objectFit: "cover", width: "100%", height: "100%" }}
+            />
+          </Card>
+        </Grid>
+
+        {/* 4th Image (Corrected placement - Below 2nd & 3rd, beside 1st Image) */}
+        <Grid item xs={12} sm={6} md={6} lg={6}>
+          <Card sx={{ borderRadius: 2, overflow: "hidden" }}>
+            <CardMedia
+              component="img"
+              image={img4}
+              alt="Gallery Image 4"
+              sx={{ objectFit: "cover", width: "100%", height: "100%" }}
+            />
+          </Card>
+        </Grid>
       </Grid>
     </Box>
   );
