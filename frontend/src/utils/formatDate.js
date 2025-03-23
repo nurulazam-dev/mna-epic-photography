2f8658bafa7c7ae73282e73359f247ebc20056a1
mnaofficialbd <mna.official92@gmail.com>
2025-03-23 22:31:37 +0600

    formatDate update

--- frontend/src/utils/formatDate.js
+++ frontend/src/utils/formatDate.js
@@ -1,5 +1,5 @@
 export const formatDate = (date, config) => {
-  const defaultOptions = { day: "2-digit", month: "2-digit", year: "numeric" };
+  const defaultOptions = { day: "2-digit", month: "2-digit", year: "2-digit" };
   const options = config ? config : defaultOptions;
   return new Date(date).toLocaleDateString("en-GB", options);
 };
