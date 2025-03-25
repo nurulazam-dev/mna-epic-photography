905891d67a28bf7d265f85b7863c2b76a4a32971
mnaofficialbd <mna.official92@gmail.com>
2025-03-25 21:21:46 +0600

    PaginationComponent update

--- frontend/src/components/Shared/PaginationComponent.jsx
+++ frontend/src/components/Shared/PaginationComponent.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+import { Box, Pagination } from "@mui/material";
 
-const PaginationComponent = () => {
-  return <div></div>;
+const PaginationComponent = ({
+  totalItems,
+  itemsPerPage,
+  currentPage,
+  onPageChange,
+}) => {
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  return (
+    <Box display="flex" justifyContent="center" mt={2}>
+      <Pagination
+        count={totalPages}
+        page={currentPage}
+        onChange={(event, value) => onPageChange(value)}
+        color="primary"
+      />
+    </Box>
+  );
 };
 
 export default PaginationComponent;
