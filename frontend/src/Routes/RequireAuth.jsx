9df295cdecb41b8c36a9adfd9675c15c3a411e29
mnaofficialbd <mna.official92@gmail.com>
2025-03-26 22:30:57 +0600

    RequireAuth update

--- frontend/src/Routes/RequireAuth.jsx
+++ frontend/src/Routes/RequireAuth.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+import { Navigate, useLocation } from "react-router-dom";
+import { useContext } from "react";
+import Loading from "../components/Shared/Loading";
+import { authContext } from "../context/AuthContext";
 
-const RequireAuth = () => {
-  return <div></div>;
+const RequireAuth = ({ children }) => {
+  const { user, loading } = useContext(authContext);
+  const location = useLocation();
+
+  if (loading) {
+    return <Loading />;
+  }
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+  return children;
 };
 
 export default RequireAuth;
