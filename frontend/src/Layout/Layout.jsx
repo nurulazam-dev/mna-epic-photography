2075038a3b913f544c729eb9552509728df2d264
mnaofficialbd <mna.official92@gmail.com>
2025-04-02 13:33:13 +0600

    Dashboard update

--- frontend/src/Layout/Layout.jsx
+++ frontend/src/Layout/Layout.jsx
@@ -1,15 +1,19 @@
+import { useLocation } from "react-router-dom";
 import Footer from "../components/Shared/Footer";
 import Header from "../components/Shared/Header";
 import Routers from "../Routes/Routers";
 
 const Layout = () => {
+  const location = useLocation();
+  const hideFooter = location.pathname.startsWith("/dashboard");
+
   return (
     <>
       <Header />
       <main>
         <Routers />
       </main>
-      <Footer />
+      {!hideFooter && <Footer />}
     </>
   );
 };
