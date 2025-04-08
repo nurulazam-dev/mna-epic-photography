42e13db1071091aa497458d65c54ae2e91faf73f
mnaofficialbd <mna.official92@gmail.com>
2025-04-08 14:08:08 +0600

    bookings-Dashboard update

--- frontend/src/Dashboard/Photographer/Bookings.jsx
+++ frontend/src/Dashboard/Photographer/Bookings.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import {
   Table,
   TableBody,
@@ -19,12 +18,24 @@ import { GppBad, VerifiedUser } from "@mui/icons-material";
 import { getShortEmail } from "../../utils/getShortEmail";
 import PaginationComponent from "../../components/Shared/PaginationComponent";
 import { useState } from "react";
+import { BASE_URL } from "../../../config";
+import Loading from "../../components/Shared/Loading";
+import useGetProfile from "../../hooks/useFetchData";
+import Error from "../../components/Shared/Error";
 
-const Bookings = ({ bookings }) => {
+const Bookings = () => {
+  const {
+    data: bookings,
+    loading,
+    error,
+  } = useGetProfile(`${BASE_URL}/photographers/profile/me`);
+
+  console.log(bookings?.bookings);
+
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 4;
   const startIndex = (currentPage - 1) * itemsPerPage;
-  const paginatedBookings = bookings?.slice(
+  const paginatedBookings = bookings?.bookings?.slice(
     startIndex,
     startIndex + itemsPerPage
   );
@@ -42,106 +53,111 @@ const Bookings = ({ bookings }) => {
           fontFamily: "serif",
         }}
       >
-        Bookings ({bookings?.length})
+        Bookings ({bookings?.bookings?.length})
       </Typography>
 
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow sx={{ backgroundColor: "#f3f3f3" }}>
-              <TableCell align="center">Client</TableCell>
-              <TableCell align="center">Phone</TableCell>
-              <TableCell align="center">Payment</TableCell>
-              <TableCell align="center">B.Status</TableCell>
-              <TableCell align="center">Price</TableCell>
-              <TableCell align="center">Booked on</TableCell>
-              <TableCell align="center">Program</TableCell>
-            </TableRow>
-          </TableHead>
+      {loading && !error && <Loading />}
 
-          <TableBody>
-            {paginatedBookings?.map((item) => (
-              <TableRow key={item?._id} hover>
-                <TableCell sx={{ padding: "2px 10px" }}>
-                  <Box display="flex" alignItems="center" gap={2}>
-                    <Avatar src={item?.user?.photo} alt={item?.user?.name} />
-                    <Box>
-                      <Typography fontWeight="bold" display="flex">
-                        {item?.user?.name}
-                        {item?.user?.isVerified === true ? (
-                          <Box
-                            display="flex"
-                            alignItems="center"
-                            justifyContent="center"
-                            color="green"
-                            height={16}
-                            width={16}
-                            sx={{ marginLeft: "3px" }}
-                          >
-                            <VerifiedUser fontSize="10px" />
-                            {/* <CheckCircleIcon fontSize="10px" /> */}
-                          </Box>
-                        ) : (
-                          <Box
-                            display="flex"
-                            alignItems="center"
-                            justifyContent="center"
-                            color="red"
-                            height={16}
-                            width={16}
-                            sx={{ marginLeft: "3px" }}
-                          >
-                            <GppBad fontSize="10px" />
-                            {/* <CancelIcon fontSize="10px" /> */}
-                          </Box>
-                        )}
-                      </Typography>
-                      <Typography variant="body2" color="textSecondary">
-                        {getShortEmail(item?.user?.email)}
-                      </Typography>
-                    </Box>
-                  </Box>
-                </TableCell>
+      {error && !loading && <Error errMessage={error} />}
 
-                <TableCell align="center">{item?.user?.phone}</TableCell>
-
-                <TableCell align="center">
-                  {item?.isPaid ? (
-                    <Box
-                      display="flex"
-                      alignItems="center"
-                      justifyContent="center"
-                      color="green"
-                    >
-                      <CheckCircleIcon sx={{ mr: 1 }} /> Paid
-                    </Box>
-                  ) : (
-                    <Box
-                      display="flex"
-                      alignItems="center"
-                      justifyContent="center"
-                      color="red"
-                    >
-                      <CancelIcon sx={{ mr: 1 }} /> Unpaid
-                    </Box>
-                  )}
-                </TableCell>
-
-                <TableCell align="center">{item?.status}</TableCell>
-                <TableCell align="center">$ {item?.servicePrice}</TableCell>
-
-                <TableCell align="center">
-                  {formatDate(item?.createdAt)}
-                </TableCell>
-                <TableCell align="center">
-                  {formatDate(item?.programDate)}
-                </TableCell>
+      {!loading && !error && (
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow sx={{ backgroundColor: "#f3f3f3" }}>
+                <TableCell align="center">Client</TableCell>
+                <TableCell align="center">Phone</TableCell>
+                <TableCell align="center">Payment</TableCell>
+                <TableCell align="center">B.Status</TableCell>
+                <TableCell align="center">Price</TableCell>
+                <TableCell align="center">Booked on</TableCell>
+                <TableCell align="center">Program</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
 
+            <TableBody>
+              {paginatedBookings?.map((item) => (
+                <TableRow key={item?._id} hover>
+                  <TableCell sx={{ padding: "2px 10px" }}>
+                    <Box display="flex" alignItems="center" gap={2}>
+                      <Avatar src={item?.user?.photo} alt={item?.user?.name} />
+                      <Box>
+                        <Typography fontWeight="bold" display="flex">
+                          {item?.user?.name}
+                          {item?.user?.isVerified === true ? (
+                            <Box
+                              display="flex"
+                              alignItems="center"
+                              justifyContent="center"
+                              color="green"
+                              height={16}
+                              width={16}
+                              sx={{ marginLeft: "3px" }}
+                            >
+                              <VerifiedUser fontSize="10px" />
+                              {/* <CheckCircleIcon fontSize="10px" /> */}
+                            </Box>
+                          ) : (
+                            <Box
+                              display="flex"
+                              alignItems="center"
+                              justifyContent="center"
+                              color="red"
+                              height={16}
+                              width={16}
+                              sx={{ marginLeft: "3px" }}
+                            >
+                              <GppBad fontSize="10px" />
+                              {/* <CancelIcon fontSize="10px" /> */}
+                            </Box>
+                          )}
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary">
+                          {getShortEmail(item?.user?.email)}
+                        </Typography>
+                      </Box>
+                    </Box>
+                  </TableCell>
+
+                  <TableCell align="center">{item?.user?.phone}</TableCell>
+
+                  <TableCell align="center">
+                    {item?.isPaid ? (
+                      <Box
+                        display="flex"
+                        alignItems="center"
+                        justifyContent="center"
+                        color="green"
+                      >
+                        <CheckCircleIcon sx={{ mr: 1 }} /> Paid
+                      </Box>
+                    ) : (
+                      <Box
+                        display="flex"
+                        alignItems="center"
+                        justifyContent="center"
+                        color="red"
+                      >
+                        <CancelIcon sx={{ mr: 1 }} /> Unpaid
+                      </Box>
+                    )}
+                  </TableCell>
+
+                  <TableCell align="center">{item?.status}</TableCell>
+                  <TableCell align="center">$ {item?.servicePrice}</TableCell>
+
+                  <TableCell align="center">
+                    {formatDate(item?.createdAt)}
+                  </TableCell>
+                  <TableCell align="center">
+                    {formatDate(item?.programDate)}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
       {bookings?.length > itemsPerPage && (
         <PaginationComponent
           totalItems={bookings?.length}
