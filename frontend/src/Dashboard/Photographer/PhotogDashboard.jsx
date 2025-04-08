d077c71fb9671efbb436fc8e04e4d5f81fbb4908
mnaofficialbd <mna.official92@gmail.com>
2025-04-08 23:28:08 +0600

    Photographer Dashboard update

--- frontend/src/Dashboard/Photographer/PhotogDashboard.jsx
+++ frontend/src/Dashboard/Photographer/PhotogDashboard.jsx
@@ -1,156 +1,147 @@
-import { useState } from "react";
-import {
-  AccountCircle,
-  PlaylistAdd,
-  Edit,
-  Warning,
-  Star,
-} from "@mui/icons-material";
-import {
-  Button,
-  Card,
-  CardContent,
-  Typography,
-  Avatar,
-  Box,
-  Alert,
-} from "@mui/material";
+import { Warning, Star, ArrowOutward } from "@mui/icons-material";
+import { Button, Typography, Avatar, Box, Alert } from "@mui/material";
 import Error from "../../components/Shared/Error";
 import Loading from "../../components/Shared/Loading";
 import useGetProfile from "../../hooks/useFetchData";
-import AboutPhotographer from "../../components/Photographers/PhotographersDetails/AboutPhotographer";
 import { BASE_URL } from "../../../config";
-import Bookings from "./Bookings";
-import PhotographerProfile from "./PhotographerProfile";
+import { Link } from "react-router-dom";
 
 const PhotogDashboard = () => {
-  const [tab, setTab] = useState("overview");
-
   const userAvatar =
     "https://p7.hiclipart.com/preview/717/24/975/computer-icons-user-profile-user-account-clip-art-avatar.jpg";
 
-  const { data, loading, error } = useGetProfile(
-    `${BASE_URL}/photographers/profile/me`
-  );
+  const {
+    data: photogData,
+    loading,
+    error,
+  } = useGetProfile(`${BASE_URL}/photographers/profile/me`);
+
+  const {
+    name,
+    photo,
+    expertise,
+    isApproved,
+    about,
+    averageRating,
+    experience,
+  } = photogData || {};
 
   return (
-    <Box maxWidth={1220} mx="auto" my={3} px={2}>
+    <Box>
       {loading && !error && <Loading />}
       {error && !loading && <Error errMessage={error} />}
 
       {!loading && !error && (
-        <Box display="flex" width="100%" mx="auto">
-          <Card sx={{ width: { xs: "25%", md: "20%" }, p: 2, mr: 2 }}>
-            <CardContent>
-              <Button
-                fullWidth
-                onClick={() => setTab("overview")}
-                startIcon={<AccountCircle />}
-                variant={tab === "overview" ? "contained" : "outlined"}
-                sx={{ mb: 2 }}
-              >
-                Overview
-              </Button>
-              <Button
-                fullWidth
-                onClick={() => setTab("bookings")}
-                startIcon={<PlaylistAdd />}
-                variant={tab === "bookings" ? "contained" : "outlined"}
-                sx={{ mb: 2 }}
-              >
-                Bookings
-              </Button>
-              <Button
-                fullWidth
-                onClick={() => setTab("settings")}
-                startIcon={<Edit />}
-                variant={tab === "settings" ? "contained" : "outlined"}
-                sx={{ mb: 2 }}
-              >
-                Profile
-              </Button>
-              <Button
-                fullWidth
-                variant="contained"
-                color="error"
-                sx={{ mt: 6 }}
-              >
-                Delete Account
-              </Button>
-            </CardContent>
-          </Card>
+        <Box px={2}>
+          {isApproved === "pending" && (
+            <Alert severity="warning" icon={<Warning />} sx={{ my: 2 }}>
+              To get approval, please complete your profile. We&apos;ll review
+              manually and approve within 3 days.
+            </Alert>
+          )}
 
-          <Box flex={1} px={2}>
-            {data?.isApproved === "pending" && (
-              <Alert severity="warning" icon={<Warning />} sx={{ my: 2 }}>
-                To get approval, please complete your profile. We&apos;ll review
-                manually and approve within 3 days.
-              </Alert>
-            )}
-
-            {tab === "overview" && (
+          <Box>
+            <Typography
+              variant="h4"
+              sx={{
+                backgroundColor: "green",
+                color: "white",
+                textAlign: "center",
+                py: 1,
+                fontFamily: "serif",
+              }}
+            >
+              About {name}
+            </Typography>
+            <Box
+              display="flex"
+              flexDirection={{ xs: "column", md: "row" }}
+              alignItems="center"
+              gap={2}
+              pt={1}
+            >
+              <Avatar
+                src={photo || userAvatar}
+                sx={{ width: 120, height: 120 }}
+              />
               <Box>
-                <Typography
-                  variant="h4"
-                  sx={{
-                    backgroundColor: "green",
-                    color: "white",
-                    textAlign: "center",
-                    py: 1,
-                    fontFamily: "serif",
-                  }}
-                >
-                  About {data?.name}
-                </Typography>
-                <Box
-                  display="flex"
-                  flexDirection={{ xs: "column", md: "row" }}
-                  alignItems="center"
-                  gap={2}
-                  pt={1}
-                >
-                  <Avatar
-                    src={data?.photo || userAvatar}
-                    sx={{ width: 120, height: 120 }}
-                  />
-                  <Box>
-                    <Typography variant="h6">{data.name}</Typography>
-                    <Typography color="textSecondary" fontSize={14}>
-                      {data.expertise + " Photography Expert" || (
-                        <Typography color="error">
-                          Expertise profile isn&apos;t updated. Please update
-                          your expertise profile.
-                        </Typography>
-                      )}
+                <Typography variant="h6">{name}</Typography>
+                <Typography color="textSecondary" fontSize={14}>
+                  {expertise + " Photography Expert" || (
+                    <Typography color="error">
+                      Expertise profile isn&apos;t updated. Please update your
+                      expertise profile.
                     </Typography>
-                    <Box display="flex" alignItems="center" gap={1} mt={1}>
-                      {<Star />}
-                      <Typography>
-                        {data?.averageRating > 0
-                          ? data?.averageRating?.toFixed(1)
-                          : data?.averageRating}
-                      </Typography>
-                    </Box>
-                    {/* <Typography fontSize={15} color="textSecondary" mt={1}>
-                      {data?.bio}
-                    </Typography> */}
-                  </Box>
+                  )}
+                </Typography>
+                <Box display="flex" alignItems="center" gap={1} mt={1}>
+                  {<Star />}
+                  <Typography>
+                    {averageRating > 0
+                      ? averageRating?.toFixed(1)
+                      : averageRating}
+                  </Typography>
                 </Box>
-                <AboutPhotographer
-                  name={data?.name}
-                  about={data?.about}
-                  experience={data?.experience}
-                />
               </Box>
-            )}
+            </Box>
 
-            {tab === "bookings" && <Bookings bookings={data?.bookings} />}
-            {tab === "settings" && (
-              <PhotographerProfile photographerData={data} />
-            )}
+            <Box mb={2}>
+              <Box>
+                <Typography variant="body1" fontSize={{ xs: 14, lg: 16 }}>
+                  {about ? (
+                    about
+                  ) : (
+                    <Typography
+                      color="error"
+                      sx={{ animation: "pulse 1s infinite" }}
+                    >
+                      Bio/About profile isn&apos;t updated. Please update your
+                      bio/about profile.
+                    </Typography>
+                  )}
+                </Typography>
+              </Box>
+
+              <Box mt={3}>
+                <Typography variant="h6" fontWeight="bold" color="primary">
+                  Experience
+                </Typography>
+                {experience ? (
+                  experience + " years of professional experience."
+                ) : (
+                  <Typography
+                    color="error"
+                    sx={{ animation: "pulse 1s infinite" }}
+                  >
+                    Experience profile isn&apos;t updated. Please update your
+                    experience profile.
+                  </Typography>
+                )}
+              </Box>
+            </Box>
           </Box>
         </Box>
       )}
+
+      <Box sx={{ px: 2 }}>
+        <Link
+          to={`/dashboard/photog-profile`}
+          style={{ textDecoration: "none" }}
+        >
+          <Button
+            variant="contained"
+            size="large"
+            sx={{
+              bgcolor: "black",
+              color: "white",
+              "&:hover": { bgcolor: "darkgreen" },
+            }}
+            endIcon={<ArrowOutward />}
+          >
+            Update Account
+          </Button>
+        </Link>
+      </Box>
     </Box>
   );
 };
