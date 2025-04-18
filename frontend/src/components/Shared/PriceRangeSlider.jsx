40eaa63a5668dce1af6c31e1ca5ad94cad264603
mnaofficialbd <mna.official92@gmail.com>
2025-04-18 19:15:34 +0600

    PriceRangeSlider update

--- frontend/src/components/Shared/PriceRangeSlider.jsx
+++ frontend/src/components/Shared/PriceRangeSlider.jsx
@@ -1,23 +1,39 @@
 /* eslint-disable react/prop-types */
-import { Box, Slider, Typography } from "@mui/material";
+import { Box, TextField } from "@mui/material";
 
 const PriceRangeSlider = ({ servicePriceRange, setServicePriceRange }) => {
-  const handleChange = (event, newValue) => {
-    setServicePriceRange(newValue);
+  const handleInputChange = (index, value) => {
+    const newRange = [...servicePriceRange];
+    newRange[index] = value === "" ? "" : Number(value);
+    setServicePriceRange(newRange);
   };
 
   return (
-    <Box display="flex" alignItems="center" gap={2} width="100%" maxWidth={260}>
-      <Typography>${servicePriceRange[0]}</Typography>
-      <Slider
-        value={servicePriceRange}
-        onChange={handleChange}
-        valueLabelDisplay="auto"
-        min={0}
-        max={1000}
-        sx={{ flexGrow: 1 }}
-      />
-      <Typography>${servicePriceRange[1]}</Typography>
+    <Box
+      display="flex"
+      flexDirection="column"
+      gap={2}
+      width="100%"
+      maxWidth={360}
+    >
+      <Box display="flex" gap={2}>
+        <TextField
+          label="Min Price"
+          type="number"
+          value={servicePriceRange[0]}
+          onChange={(e) => handleInputChange(0, e.target.value)}
+          size="small"
+          fullWidth
+        />
+        <TextField
+          label="Max Price"
+          type="number"
+          value={servicePriceRange[1]}
+          onChange={(e) => handleInputChange(1, e.target.value)}
+          size="small"
+          fullWidth
+        />
+      </Box>
     </Box>
   );
 };
